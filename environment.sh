d5393f321a0e1a1d8a059042fb74adcc7368ddcd
wenchaokk <wenqin@ucdavis.edu>
2025-08-03 16:31:01 -0700

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,2 +1,2 @@
 export TENSORZERO_CLICKHOUSE_URL="http://chuser:chpassword@localhost:8123/tensorzero"
-export LLAMA_API_KEY="LLM|1098418455551636|hXMuWuSi2wCLwrq3KX63hFPfX7o"
+export LLAMA_API_KEY=""
